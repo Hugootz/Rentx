9998b669e83998c2ece21c280056af9118d8ebaa
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-02-04 11:57:28 -0300

    Tela 2 detalhes do carro e seu CSS

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,19 +1,80 @@
 import React from "react";
 import { BackButton } from "../../components/BackButton";
 import { ImageSlider } from "../../components/ImageSlider";
-import { Container, Header } from "./styles";
+import { Accessory } from "../../components/Accessory";
+import { getAccessoriesIcon } from "../../Utils/getAccessoriesIcon";
+import { Button } from "../../components/Button";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Accessories,
+  Footer,
+} from "./styles";
+import { Home } from "../Home";
+import { CarDTO } from "../../dtos/carDTO";
+
+interface Params {
+  car: CarDTO;
+}
 
 export function CarDetails() {
+  const navigation = useNavigation<any>();
+  const route = useRoute();
+  const { car } = route.params as Params;
+
+  function handleConfirmRental() {
+    navigation.navigate("Scheduling", { car });
+  }
+  function handleBack() {
+    navigation.goBack();
+  }
   return (
     <Container>
       <Header>
-        <BackButton onPress={() => {}} />
+        <BackButton onPress={handleBack} />
       </Header>
-      <ImageSlider
-        imagesUrl={[
-          "https://i.pinimg.com/originals/0b/7b/31/0b7b31c91bc9fd14fa0e5620bdb80731.png",
-        ]}
-      />
+      <CarImages>
+        <ImageSlider imagesUrl={car.photos} />
+      </CarImages>
+      <Content>
+        <Details>
+          <Description>
+            <Brand>{car.brand}</Brand>
+            <Name>{car.name}</Name>
+          </Description>
+          <Rent>
+            <Period>{car.rent.period}</Period>
+            <Price>R$ {car.rent.price}</Price>
+          </Rent>
+        </Details>
+        <Accessories>
+          {car.accessories.map((accessories) => (
+            <Accessory
+              key={accessories.type}
+              name={accessories.name}
+              icon={getAccessoriesIcon(accessories.type)}
+            />
+          ))}
+        </Accessories>
+        <About>{car.about}</About>
+      </Content>
+      <Footer>
+        <Button
+          title="Escolher período do aluguel"
+          onPress={handleConfirmRental}
+        />
+      </Footer>
     </Container>
   );
 }
